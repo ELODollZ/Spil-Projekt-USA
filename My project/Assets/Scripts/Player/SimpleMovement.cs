b400fe6f502d797528b6df4b8717932163353e89
SauperGamer <61630124+SauperGamer@users.noreply.github.com>
2022-02-10 13:40:40 -0800

    Prof of consept

---
+++ My project/Assets/Scripts/Player/SimpleMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Af Rasmus
+
+public class SimpleMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    ParticleSystem particleSystem;
+
+    Rigidbody2D rb2d;
+
+    [SerializeField]
+    float disBetweenStep;
+
+    float disToNextStep;
+
+    Vector2 lastPos;
+
+    void Start()
+    {
+        disToNextStep = disBetweenStep;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    
+    void Update()
+    {
+        Vector2 move = new Vector2(); ;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.y += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.y -= speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x += speed;
+        }
+
+        disToNextStep -= Vector2.Distance(lastPos, (Vector2)transform.position);
+        lastPos = transform.position;
+
+        rb2d.velocity = move;
+
+        if (disToNextStep <= 0)
+        {
+            disToNextStep = disBetweenStep;
+            particleSystem.Play();
+        }
+
+    }
+}
