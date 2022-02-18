ce87a1d5d83b2f1bc4a76e3efae698dd5e3f5206
SauperGamer <61630124+SauperGamer@users.noreply.github.com>
2022-02-18 10:25:51 -0800

    Stuff

--- My project/Assets/Scripts/Audio/PlayAudioWhenPing.cs
+++ My project/Assets/Scripts/Audio/PlayAudioWhenPing.cs
@@ -9,6 +9,14 @@ public class PlayAudioWhenPing : MonoBehaviour
 {
     AudioSource audioSource;
 
+    [SerializeField]
+    AudioClip[] sounds;
+
+    [SerializeField]
+    SoundPlayMode soundPlayMode = SoundPlayMode.none;
+
+    int next = 0;
+
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -17,7 +25,27 @@ public class PlayAudioWhenPing : MonoBehaviour
     //spiler en lyd med en vis lydstyrke
     public void PlayAudio(float power)
     {
+        //hvilken lyd der skal spilles
+        switch (soundPlayMode)
+        {
+            case SoundPlayMode.random:
+                audioSource.clip = sounds[Random.Range(0, sounds.Length)];
+                break;
+            case SoundPlayMode.inOrder:
+                audioSource.clip = sounds[next];
+                next++;
+                if (next == sounds.Length) next = 0;
+                break;
+            case SoundPlayMode.none:
+                break;
+            default:
+                break;
+        }
+
         audioSource.volume = power;
         audioSource.Play();
     }
 }
+
+
+public enum SoundPlayMode {random, inOrder,/* bySoundPower,*/ none}
