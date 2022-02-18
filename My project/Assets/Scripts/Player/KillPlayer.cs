da5a5852caf256375780875a10f57dd0670c68dc
Daniel <DanielDKN29@gmail.com>
2022-02-18 23:13:49 +0100

    Kommentar til kode
    kommentar til killPlayer

--- My project/Assets/Scripts/Player/KillPlayer.cs
+++ My project/Assets/Scripts/Player/KillPlayer.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Af Daniel Nybo
 public class KillPlayer : MonoBehaviour
 {
+    //kalder variabler som endelige ikke bliver brugte, det var ment til hvis vi også skulle fjerne rigidbody2D og scripts for at undgå errors når spilleren døde
     GameObject player;
     GameObject enemy;
     Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
+        //kalder rigidbodyen så jeg kunne have brugte den senere.
         GetComponent<Rigidbody2D>();   
     }
 
@@ -18,11 +21,12 @@ public class KillPlayer : MonoBehaviour
     {
         
     }
-
+    //laver en function der tjekker hvad object rammes af, og så om objectet har tag Enemy og hvis den har køre den if statement.
     public void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("enemy"))
         {
+            //debug er for at se at spilleren er død.
             Debug.Log("The Player has entered Death!");
             Destroy(player);
         }
