9fb6ab86e94d8ee615bcae3ef19a75ab02894c43
Gu4rdiaN PP <tobiashalberg27@gmail.com>
2022-02-22 09:49:58 -0800

    Sat limit på hvor mange sten man kan kaste

--- My project/Assets/Scripts/Player/KasteStenStuff/Throwing.cs
+++ My project/Assets/Scripts/Player/KasteStenStuff/Throwing.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //Made By Editor: Daniel M�nster Nybo (shooting/throwing scripts)
+// Ændring af Tobias
+// Ændring: Har sat et limit på hvor mange sten man kan kaste
 public class Throwing : MonoBehaviour
 {
     //Skaber variabler til at kalde i scripts, og sætter hastighed på speed, 
@@ -11,24 +13,41 @@ public class Throwing : MonoBehaviour
     public float throwingSpeed = 10;
     Vector2 lookDirection;
     float lookAngle;
+
+    [SerializeField] private int CurrentRock;
+    [SerializeField] private int MaxRock = 4;
+    private int MinRock = 0;
+
+    void Start()
+    {
+        CurrentRock = 1;  
+    }
     void Update()
     {
         //Ved Space køres funktion af at kalde et objectclone Rock og sætter den ved firepoint.right og så en debug til udskrive i consolen at den faktisk kan tjekke om der bliver trykket på Space
         //Kalder også Rigidbody og sætter en veolcity på Rock clonerne som indsætters ved firePoint.
         if (Input.GetMouseButtonDown(0))
         {
-            //Sætter variablen lookDirection til main Camera og bruger Math til at udregne position til LookDirection og omregner fra radians til degres
-            lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - firePoint.position;
-            //lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+            if (CurrentRock > MinRock)
+            {
+                //Sætter variablen lookDirection til main Camera og bruger Math til at udregne position til LookDirection og omregner fra radians til degres
+                lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - firePoint.position;
+                //lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
 
-            firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
+                firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
 
-            GameObject rockClone = Instantiate(rock);
-            rockClone.transform.position = firePoint.position;
-            rockClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle);
+                GameObject rockClone = Instantiate(rock);
+                rockClone.transform.position = firePoint.position;
+                rockClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle);
 
-            rockClone.GetComponent<Rigidbody2D>().velocity = lookDirection.normalized * throwingSpeed;
+                rockClone.GetComponent<Rigidbody2D>().velocity = lookDirection.normalized * throwingSpeed;
 
+                CurrentRock --;
+            }
+            else
+            {
+                Debug.Log("Out of ammo");
+            }
         }
     }
 
