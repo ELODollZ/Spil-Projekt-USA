2d29ee4b63280ffd8c84630b46dd8a2e8e786c74
SauperGamer <61630124+SauperGamer@users.noreply.github.com>
2022-02-24 11:22:48 -0800

    UI Ændring er

---
+++ My project/Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menu;
+
+
+    bool pauseState = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (pauseState)
+            {
+                Pause(false);
+            }
+            else
+            {
+                Pause(true);
+            }
+        }
+    }
+
+
+    public void Pause(bool pause)
+    {
+        menu.SetActive(pause);
+        pauseState = pause;
+        if (pauseState)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+}
